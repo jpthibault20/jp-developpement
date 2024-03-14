82b6327c20a19e57705a1ecc0ab0e29366c26f27
jpthibault20 <tjeanpierre757@gmail.com>
2024-03-14 17:58:03 +0100

    feat add navbar with not logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,83 +1,97 @@
-"use client"
+import React from "react";
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo.jsx";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import Image from 'next/image'
+export default function App() {
+  const menuItems = [
+    "Accueil",
+    "Services",
+    "Developpeurs",
+    "Contact",
 
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  ];
 
   return (
-    <nav className="">
-      <div className="max-w-full m-5 flex justify-between">
-        <div className="flex-shrink-0">
-          <Link href="/">
-            <Image src="/logoSombre.png" width={50} height={50} alt="Logo" />
+    <Navbar disableAnimation
+      style={{
+        backgroundColor: "transparent", // Définit un arrière-plan transparent
+      }}
+    >
+
+
+
+      <NavbarContent className="sm:hidden pr-3" justify="start">
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">JP-Developpement</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+
+
+      <NavbarContent className="hidden sm:flex gap-4" >
+        <NavbarBrand className="flex justify-start">
+          <AcmeLogo />
+          <p className="font-bold text-inherit">JP-Developpement</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="start">
+        <NavbarItem>
+          <Link color="foreground" href="/">
+            Accueil
           </Link>
-        </div>
-        <div className="bg-white rounded-full mr-20 flex items-center shadow-xl px-2">
-          <div className="flex space-x-4">
-            <Link href="/" className="hover:bg-gray-200 hover:font-semibold px-3 py-2 rounded-full">
-              Accueil
-            </Link>
-            <Link href="/services" className="hover:bg-gray-200 hover:font-semibold px-3 py-2 rounded-full">
-              Services
-            </Link>
-            <Link href="/developer" className="hover:bg-gray-200 hover:font-semibold px-3 py-2 rounded-full">
-              Developpeurs
-            </Link>
-            <Link href="/contact" className="hover:bg-gray-200 hover:font-semibold px-3 py-2 rounded-full">
-              Contact
-            </Link>
-          </div>
-        </div>
-      </div>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/services">
+            Services
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/developer">
+            Developpeurs
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="/contact">
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
 
-      {/* Volet latéral pour les petits écrans */}
-      <div
-        className={`${isOpen ? 'translate-x-0' : 'translate-x-full'
-          } fixed inset-y-0 right-0 z-50 w-3/4 bg-gray-800 transition duration-300 ease-in-out md:hidden`}
-      >
-        <div className="px-4 py-6">
-          <div className="flex justify-end">
-            <button
-              onClick={toggleMenu}
-              type="button"
-              className="text-gray-300 hover:text-white focus:outline-none"
-            >
-              <FontAwesomeIcon icon={faTimes} className="h-6 w-6" />
-            </button>
-          </div>
-          <div className="mt-6 space-y-4">
-            <Link
-              href="/"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Accueil
-            </Link>
-            <Link
-              href="/about"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              À propos
-            </Link>
-            <Link
-              href="/contact"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >
-              Contact
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+
+
+      <NavbarMenu>
+        <NavbarMenuItem className="mt-5">
+          <Link color="foreground" href="/">
+            Accueil
+          </Link>
+        </NavbarMenuItem>
+
+        <NavbarMenuItem className="mt-5">
+          <Link color="foreground" href="/services">
+            Services
+          </Link>
+        </NavbarMenuItem>
+
+        <NavbarMenuItem className="mt-5">
+          <Link color="foreground" href="/developer">
+            Developpeurs
+          </Link>
+        </NavbarMenuItem>
+
+        <NavbarMenuItem className="mt-5">
+          <Link color="foreground" href="/contact">
+            Contact
+          </Link>
+        </NavbarMenuItem>
+
+      </NavbarMenu>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
