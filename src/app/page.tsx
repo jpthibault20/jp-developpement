b82535fedc39af0ef00e07bf4408015398cfdd06
jpthibault20 <tjeanpierre757@gmail.com>
2024-03-15 10:56:25 +0100

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
+import { Button } from "@nextui-org/react";
 
 const home = () => {
-  
+
   return (
     <>
-      <Navbar activePage={"accueil"}/>
-      <h1 className=''>welcome home page</h1>
-      
+      <Navbar activePage={"accueil"} />
+
+      <h1 className='text-center mt-20 text-3xl'>Qui Sommes Nous ?</h1>
+
+      <p className='m-10 lg:mx-60 leading-relaxed text-justify'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Hac habitasse platea dictumst vestibulum.
+        Mauris vitae ultricies leo integer malesuada. Imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque. Eget mauris pharetra et ultrices.
+        In massa tempor nec feugiat nisl pretium fusce id. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant.
+        Ultricies tristique nulla aliquet enim tortor at auctor.
+        Nibh mauris cursus mattis molestie a iaculis. Ac felis donec et odio pellentesque diam volutpat commodo sed.
+      </p>
+
+      <div className='lg:place-content-center lg:grid lg:grid-cols-2 lg:gap-48 lg:mx-auto place-content-center grid grid-cols-2 gap-10 mx-auto'>
+        <Button radius="full" className="bg-[#4f46e5] text-white drop-shadow-xl">
+          Contactez-nous
+        </Button>
+
+        <Button radius="full" className="bg-[#4f46e5] text-white drop-shadow-xl">
+          Obtenir un devis
+        </Button>
+      </div>
+
+
+      <div className="w-full  bg-[#4f46e5] mt-20 text-[#c7d2fe]">
+        <h1 className='text-center mt-10 text-3xl'>Nos Compétences</h1>
+
+        <p className='m-10 lg:mx-60 leading-relaxed text-justify'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Eu turpis egestas pretium aenean pharetra. Porttitor massa id neque aliquam vestibulum morbi blandit cursus. Semper feugiat nibh sed pulvinar proin.
+          Condimentum id venenatis a condimentum vitae sapien pellentesque habitant. Donec massa sapien faucibus et molestie ac feugiat.
+          Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Orci a scelerisque purus semper eget. Lobortis elementum nibh tellus molestie nunc non blandit.
+          Ipsum a arcu cursus vitae congue mauris.
+        </p>
+
+
+
+      </div>
+
     </>
   )
 }
 
-export default home
+export default home;
