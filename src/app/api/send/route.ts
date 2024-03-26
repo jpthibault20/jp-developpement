fac14374cdec9117c32fe7a46269c71a9d9c2de9
jpthibault20 <tjeanpierre757@gmail.com>
2024-03-26 15:55:20 +0100

    update post

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,9 +1,10 @@
+import { NextRequest } from 'next/server';
 import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: { json: () => any }) {
+export async function POST(request: NextRequest): Promise<Response> {
   try {
     const requestBody = await request.json();
     const {Firstname, Lastname, Phone, Email, Message } = requestBody;
